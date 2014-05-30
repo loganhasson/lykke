7a003699ca828c195b905e581b6814bbeddd7627
Logan Hasson <logan.hasson@gmail.com>
2014-05-30 14:04:14 -0400

    Set up 4.1, figaro, db, airbrake, GA

---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+working_directory "/home/loganhasson/lykke/current"
+pid "/home/loganhasson/lykke/shared/pids/unicorn.pid"
+stderr_path "/home/loganhasson/lykke/shared/log/unicorn.log"
+stdout_path "/home/loganhasson/lykke/shared/log/unicorn.log"
+worker_processes 2
+timeout 30
