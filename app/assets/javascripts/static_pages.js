a75939a020ba131578866d8ea9b0b84e70e59d7a
Logan Hasson <logan.hasson@gmail.com>
2014-05-30 22:35:31 -0400

    Clean up some facebook crap

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,11 @@
+$(function() {
+  function fixFacebookUgliness() {
+    if (window.location.hash == '#_=_') {
+      history.replaceState 
+        ? history.replaceState(null, null, window.location.href.split('#')[0])
+        : window.location.hash = '';
+    };   
+  };
+
+  fixFacebookUgliness();
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
